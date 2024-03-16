984d6a6a72e6989f0a503adeb43450031b75cb15
Kim SeungHyun <korafa@naver.com>
2024-03-16 09:02:10 +0900

    초기 커밋.

---
+++ ArrayTestWithGPT/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace ArrayTestWithGPT
+{
+    internal class Program
+    {
+        public static int[] intNums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        static void Main(string[] args)
+        {
+            int evenSum = EvenNumsSum();
+            int oddSum = OddNumsSum();
+
+            Console.WriteLine($"모든 짝수들의 합은 {evenSum}, 모든 홀수들의 합은 {oddSum}");
+        }
+
+        public static int EvenNumsSum()
+        {
+            int evenNumSum = 0;
+            for (int i = 0; i < intNums.Length; i++)
+            {
+                if (intNums[i] % 2 == 0)
+                {
+                    evenNumSum += intNums[i];
+                }
+            }
+
+            return evenNumSum;
+        }
+
+        public static int OddNumsSum()
+        {
+            int oddNumSum = 0;
+            for (int i = 0; i < intNums.Length; i++)
+            {
+                if (intNums[i] % 2 == 1)
+                {
+                    oddNumSum += intNums[i];
+                }
+            }
+
+            return oddNumSum;
+        }
+    }
+}
